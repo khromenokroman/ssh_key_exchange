4ba3c1ad98c5922514dbce3a93a4a4ea987318c4
khromenokroman <roma55592@yandex.ru>
2024-08-27 11:00:14 +0200

    Add user argument and save public key to user's .ssh directory
    Enhanced the client to accept a username argument and save the received public key to the specified user's .ssh directory. Also added argument parsing and main function to structure the client script.

--- client.py
+++ client.py
@@ -1,16 +1,38 @@
+#!/usr/bin/python3
 import socket
+import argparse
+import os
 
 
-def get_public_key(ip, port):
+def save_public_key(user, key):
+    # Создание .ssh директории в случае отсутствия
+    os.makedirs(f'/home/{user}/.ssh', exist_ok=True)
+    with open(f'/home/{user}/.ssh/received_key.pub', 'w') as file:
+        file.write(key)
+
+
+def get_public_key(ip, port, user):
     # Создание сокета
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((ip, port))
 
         # Получение данных (публичного ключа) от сервера
-        data = s.recv(1024)
+        data = s.recv(1024).decode()
 
-    print('Received', repr(data))
+    print(f'Received public key for {user}:', repr(data))
+
+    # Сохранение полученного публичного ключа
+    save_public_key(user, data)
 
 
-# Замените 'localhost' на IP-адрес сервера, если он запущен на другой машине
-get_public_key('localhost', 1993)
+def main():
+    parser = argparse.ArgumentParser(description="Receive public key from server.")
+    parser.add_argument("user", help="username to save received public key")
+    args = parser.parse_args()
+
+    # Замените 'localhost' на IP вашего сервера, если он запущен на другой машине
+    get_public_key('localhost', 1993, args.user)
+
+
+if __name__ == "__main__":
+    main()
