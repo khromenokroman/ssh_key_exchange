84b238680935872a79409a359e7b759d45a223aa
khromenokroman <roma55592@yandex.ru>
2024-08-27 10:55:43 +0200

    Add user-specific public key retrieval and signal handling.
    This update introduces functionality to retrieve user-specific public keys and send them to connected clients. Additionally, it includes signal handling for graceful server shutdown. Improved command line argument handling was also implemented.

--- server.py
+++ server.py
@@ -1,25 +1,52 @@
 import socket
+import argparse
+import os
+import signal
+import sys
 
-# Замените это на ваш публичный ключ
-public_key = """-----BEGIN PUBLIC KEY-----
-...Your public key here...
------END PUBLIC KEY-----"""
+
+def get_public_key(user):
+    try:
+        with open(f'/home/{user}/.ssh/id_rsa.pub', 'r') as file:
+            public_key = file.read()
+        return public_key
+    except FileNotFoundError:
+        return "Key not found"
 
 
-def start_server(port):
+def start_server(port, user):
+    # Получение публичного ключа пользователя
+    public_key = get_public_key(user)
+
     # Создание сокета
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
-        server.bind(('0.0.0.0', port))
-        server.listen()
-        print(f"Server started on port {port}")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('0.0.0.0', port))
+    server.listen()
+    print(f"Server started on port {port}")
 
-        while True:
-            # Ожидание подключения клиента
-            client, addr = server.accept()
-            with client:
-                print(f"Connected by {addr}")
-                # Отправка публичного ключа клиенту
-                client.sendall(public_key.encode())
+    def signal_handler(sig, frame):
+        print("\nServer is shutting down...")
+        server.close()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    while True:
+        # Ожидание подключения клиента
+        client, addr = server.accept()
+        with client:
+            print(f"Connected by {addr}")
+            # Отправка публичного ключа клиенту
+            client.sendall(public_key.encode())
 
 
-start_server(1993)
+def main():
+    parser = argparse.ArgumentParser(description="Send public key by server.")
+    parser.add_argument("user", help="username to find public key")
+    args = parser.parse_args()
+
+    start_server(1993, args.user)
+
+
+if __name__ == "__main__":
+    main()
